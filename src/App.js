520d34d9b08a0bc0774fb53b012fcf85c307e818
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-08-04 00:19:58 +0530

    completed ex-07

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let userName = 'Abhay';
+  let likeCounter = 0;
+
+  function myLikesCounter(){
+    likeCounter++;
+    console.log("liked" , likeCounter);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>emoji interpreter</h1>
+      <h2>welcome {userName}</h2>
+      <button onClick= {myLikesCounter}>like</button> 
     </div>
   );
 }
