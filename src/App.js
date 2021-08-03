0588bec100e98b9e24044ee5106898840bd3d071
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-08-04 00:40:31 +0530

    completed ex-08

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 
 function App() {
   let userName = 'Abhay';
-  let likeCounter = 0;
+  // let likeCounter = 0;
 
-  function myLikesCounter(){
-    likeCounter++;
-    console.log("liked" , likeCounter);
+  const [likeCounter, newLikeCounter] = useState(0);
+
+  function myLikesCounter() {
+    // mutablity and immutability
+    // let increamentedLikes = likeCounter++; 
+    // this line will gives error because we can't assign value to a constant variable "likeCounter"
+
+    let increamentedLikes = likeCounter + 1; // this will not throw error because we are incrementing the value by 1 insted of assigning it a value 
+    
+    newLikeCounter(increamentedLikes);
+    // console.log("liked", likeCounter);
   }
 
   return (
     <div className="App">
       <h1>emoji interpreter</h1>
       <h2>welcome {userName}</h2>
-      <button onClick= {myLikesCounter}>like</button> 
+      <button onClick={myLikesCounter}>like</button> {likeCounter}
     </div>
   );
 }
